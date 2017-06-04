e80a784e9baf748493052089276f2f59956652a5
mousaviarman <armnmousavi@gmail.com>
2017-06-04 09:53:17 -0700

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+
 /**
  * One Contact will store various information about one person
+ * 
  * @author Nick
  *
  */
@@ -12,91 +14,108 @@ public class Contact {
 	private String emailAddress;
 	private String phoneNumber;
 	private String notes;
+
 	/**
 	 * Asks user for information to fill in a new contact
+	 * 
 	 * @author Nick
 	 */
-	public Contact(){
+	public Contact() {
 		System.out.println("new contact constructed");
 	}
+
 	/**
 	 * Returns the contact's first name
+	 * 
 	 * @author Nick
 	 */
 	public String getFirstName() {
 		return "got test first Name";
 	}
+
 	/**
 	 * assigns the contact's first name
+	 * 
 	 * @author Nick
 	 */
 	public void setFirstName(String newFirstName) {
 	}
+
 	/**
-	 * returns the contact's last name
-	 * @author Nick
+	 * Parameterized constructor that sets lastName
+	 * 
+	 * @author armanmousavi
 	 */
-	public String getLastName() {
-		return "got test last name";
+	public Contact(String lastName) {
+		this.lastName = lastName;
 	}
-	/**
-	 * assigns the contact's last name
-	 * @author Nick
-	 */
-	public void setLastName(String newLastName) {
-	}
+
 	/**
 	 * returns the contact's street address
+	 * 
 	 * @author Nick
 	 */
 	public String getStreetAddress() {
 		return "got test Address";
 	}
+
 	/**
 	 * assigns the contact's street address
+	 * 
 	 * @author Nick
 	 */
 	public void setStreetAddress(String newStreetAddress) {
 	}
+
 	/**
 	 * returns the contact's email address
+	 * 
 	 * @author Nick
 	 */
 	public String getEmailAddress() {
 		return "got test email";
 	}
+
 	/**
 	 * assigns the contact's email address
+	 * 
 	 * @author Nick
 	 */
 	public void setEmailAddress(String newEmailAddress) {
 	}
+
 	/**
 	 * returns the contact's phone number
+	 * 
 	 * @author Nick
 	 */
 	public String getPhoneNumber() {
 		return "got test phone#";
 	}
+
 	/**
 	 * assigns the contact's phone number
+	 * 
 	 * @author Nick
 	 */
 	public void setPhoneNumber(String newPhoneNumber) {
 	}
+
 	/**
 	 * returns the contact's notes
+	 * 
 	 * @author Nick
 	 */
 	public String getNotes() {
 		return "got test notes";
 	}
+
 	/**
 	 * assigns the contact's notes
+	 * 
 	 * @author Nick
 	 */
 	public void setNotes(String newNotes) {
 	}
-	
-	
+
 }
